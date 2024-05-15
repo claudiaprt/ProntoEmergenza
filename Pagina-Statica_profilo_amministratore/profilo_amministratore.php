cb37e10eb703558f94a4d0a510c40362bdd883f6
Meluccio Karim <karim.meluccio@gmail.com>
2024-05-15 12:13:09 +0200

    Add files via upload

---
+++ Pagina-Statica_profilo_amministratore/profilo_amministratore.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/globals.css" />
+    <link rel="stylesheet" href="css/styleguide.css" />
+    <link rel="stylesheet" media="screen and (min-width:701px)" href="css/styleD_profilo_amm.css" />
+    <link rel="stylesheet" media="screen and (max-width:700px)" href="css/styleM_profilo_amm.css" />
+    <script type="text/javascript" src="js/script_profilo_amm.js"></script>
+  </head>
+  <body>
+    <div class="desktop">
+      <div class="profilo">
+        <div class="div">
+          <img class="immagine-logo" src="img/immagine-logo-ambulanza.png" />
+          <div class="pulsante-contatti"><div class="text-wrapper">Contatti</div></div>
+          <div class="pulsante-bacheca"><div class="text-wrapper">Bacheca</div></div>
+          <div class="pulsante-calendario"><div class="text-wrapper">Calendario</div></div>
+          <div class="pulsante-profilo"><div class="text-wrapper">Profilo</div></div>
+          <div class="pulsante-log-out"><div class="text-wrapper">Log out &gt;</div></div>
+          <img class="ellipse" src="img/ellipse-17.png" />
+          <div class="text-wrapper-2">Mario Rossi</div>
+          <img class="img" src="img/ellipse-18.png" />
+          <div class="text-wrapper-3">Enrica Rossi</div>
+          <img class="ellipse-2" src="img/ellipse-19.png" />
+          <div class="text-wrapper-4">Mario Bianchi</div>
+          <img class="ellipse-3" src="img/ellipse-20.png" />
+          <div class="text-wrapper-5">Elena Bianchi</div>
+          <img class="ellipse-4" src="img/ellipse-21.png" />
+          <div class="text-wrapper-6">Davide Zeni</div>
+          <img class="ellipse-5" src="img/ellipse-22.png" />
+          <div class="text-wrapper-7">Lorenzo Pozzi</div>
+          <img class="ellipse-6" src="img/ellipse-23.png" />
+          <div class="text-wrapper-8">Martina Pizzoni</div>
+          <img class="ellipse-7" src="img/ellipse-24.png" />
+          <div class="text-wrapper-9">Andrea Marchesi</div>
+          <img class="ellipse-8" src="img/ellipse-15.png" />
+          <div class="text-wrapper-10">Marzia Girelli</div>
+          <img class="ellipse-9" src="img/ellipse-16.png" />
+          <div class="text-wrapper-11">Nicola Vallini</div>
+          <img class="ellipse-10" src="img/ellipse-17.png" />
+          <div class="text-wrapper-12">Mario Rossi</div>
+          <img class="ellipse-11" src="img/ellipse-18.png" />
+          <div class="text-wrapper-13">Enrica Rossi</div>
+          <img class="ellipse-12" src="img/ellipse-19.png" />
+          <div class="text-wrapper-14">Mario Bianchi</div>
+          <img class="ellipse-13" src="img/ellipse-20.png" />
+          <div class="text-wrapper-15">Elena Bianchi</div>
+          <img class="ellipse-14" src="img/ellipse-21.png" />
+          <div class="text-wrapper-16">Davide Zeni</div>
+          <img class="ellipse-15" src="img/ellipse-22.png" />
+          <div class="text-wrapper-17">Lorenzo Pozzi</div>
+          <img class="ellipse-16" src="img/ellipse-23.png" />
+          <div class="text-wrapper-18">Martina Pizzoni</div>
+          <img class="ellipse-17" src="img/ellipse-24.png" />
+          <div class="text-wrapper-19">Andrea Marchesi</div>
+          <img class="ellipse-18" src="img/ellipse-25.png" />
+          <div class="text-wrapper-20">Marzia Girelli</div>
+          <img class="ellipse-19" src="img/ellipse-26.png" />
+          <div class="text-wrapper-21">Nicola Vallini</div>
+        </div>
+      </div>
+    </div>
+    <div class="mobile">
+      <div class="profilo">
+        <div class="div">
+          <div class="top-menu">
+            <div class="titolo-profilo"><div class="text-wrapper">Profili</div></div>
+            <div class="freccia-dx">
+              <div class="ellipse"></div>
+              <img class="vector" src="img/vector-1.svg" />
+            </div>
+            <img class="burger-menu" src="img/burger-menu.svg" onclick="showMenu()" class="show-burger"/>
+            <div class="menu-mobile">
+              <div onclick="showMenu()"></div>
+            </div>
+          </div>
+          <div class="menu-mobile">
+            <div id="menu-mobile-box" onclick="showMenu()" class>
+              <script>stampaMenu("m")</script>
+            </div>
+          </div>
+          <img class="img" src="img/ellipse-17.png" />
+          <div class="text-wrapper-2">Mario Rossi</div>
+          <img class="ellipse-2" src="img/ellipse-18.png" />
+          <div class="text-wrapper-3">Enrica Rossi</div>
+          <img class="ellipse-3" src="img/ellipse-19.png" />
+          <div class="text-wrapper-4">Mario Bianchi</div>
+          <img class="ellipse-4" src="img/ellipse-20.png" />
+          <div class="text-wrapper-5">Elena Bianchi</div>
+          <img class="ellipse-5" src="img/ellipse-21.png" />
+          <div class="text-wrapper-6">Davide Zeni</div>
+          <img class="ellipse-6" src="img/ellipse-22.png" />
+          <div class="text-wrapper-7">Lorenzo Pozzi</div>
+          <img class="ellipse-7" src="img/ellipse-23.png" />
+          <div class="text-wrapper-8">Martina Pizzoni</div>
+          <img class="ellipse-8" src="img/ellipse-24.png" />
+          <div class="text-wrapper-9">Andrea Marchesi</div>
+          <img class="ellipse-9" src="img/ellipse-25.png" />
+          <div class="text-wrapper-10">Marzia Girelli</div>
+          <img class="ellipse-10" src="img/ellipse-26.png" />
+          <div class="text-wrapper-11">Nicola Vallini</div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
