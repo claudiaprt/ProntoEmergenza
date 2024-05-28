afa8c7fe6bab373813ea665803d6ea7b93ebe389
LucaBiemmi <lucabbbdv7@gmail.com>
2024-05-29 01:16:45 +0200

    Update
    Mancanti alcune parti mobile, implementazione database tramite file funzioni generale e sessioni

--- Pagina-Statica_login/login.php
+++ Pagina-Statica_login/login.php
@@ -7,26 +7,20 @@
 	<link rel="stylesheet" media="screen and (min-width: 768px)" href="style_login_desktop.css" />
 	<link rel="stylesheet" media="screen and (max-width: 767px)" href="style_login_mobile.css" />
     <link rel="stylesheet" href="styleguide.css" />
-	<?php /*
-		$isMob = is_numeric(strpos(strtolower($_SERVER["HTTP_USER_AGENT"]), "mobile")); 
- 
-			if($isMob){ 
-				echo "<link rel='stylesheet' href='style_login_mobile.css'/>";
-			}else{ 
-				echo "<link rel='stylesheet' href='style_login_desktop.css'/>"; 
-			}*/
-	?>
+	<title>Accesso</title>
+	<script type="text/javascript" src="funz.js"></script>
 </head>
   <body>
+		<h2 id="h2Log"></h2>
 		<div class="log-in">
 		  <div class="div">
 			<img class="icona-ambulanza" src="img/icona-ambulanza.png" />
 			<img class="LOGO-ambulanza" src="img/logo-ambulanza.png" />
 			<div class="overlap-group">
-			<form action="#" method="POST" >
+			<form action="#" method="POST" name="formLog" onsubmit="return loginJS(document.formLog)">
 				<div class="bottone-accedi"><input type="submit" class="text-wrapper-2" value="Accedi"></input></div>
-				<div class="bottone-username"><input type="text" class="text-wrapper-3" placeholder="Username"></input></div>
-				<div class="div-wrapper"><input type="password" class="text-wrapper-3" placeholder="Password"></input></div>
+				<div class="bottone-username"><input type="text" class="text-wrapper-3" placeholder="Username" name="textLog"></input></div>
+				<div class="div-wrapper"><input type="password" class="text-wrapper-3" placeholder="Password" name="pswLog"></input></div>
 			</form> 
 			</div>
 		  </div>
