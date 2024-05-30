b48fa096e7d8f5e0dbe0f52dbc86dd9288dbc350
LucaBiemmi <lucabbbdv7@gmail.com>
2024-05-30 23:42:23 +0200

    Update
    Finiti ultimi cambiamenti, manca solo un problema grafico con il messaggio di errore nella parte mobile.

--- Pagina-Statica_login/ricercaUser.php
+++ Pagina-Statica_login/ricercaUser.php
@@ -1,5 +1,5 @@
 <?php
-$dbms = "mysql";
+/*$dbms = "mysql";
 $host = "127.0.0.1";
 $db_name = "pronto_emergenza";
 $db_user = "root";
@@ -49,5 +49,59 @@ try {
 } finally {
     if (isset($stmt)) $stmt = null;
     if (isset($conn)) $conn = null;
+}*/
+require_once "D:\\xampp-portable-windows-x64-8.2.4-0-VS16\\xampp\\htdocs\\Scuola\\Pagina-Statica_login\\globals.php";
+require_once "D:\\xampp-portable-windows-x64-8.2.4-0-VS16\\xampp\\htdocs\\Scuola\\Pagina-Statica_login\\db.php";  // adjust the path to where the DB class is located
+use lib\DB;
+
+header('Content-Type: application/json');
+
+$responseArray = array(
+    'risultato' => 'false',
+    'tipoErr' => 'log'
+);
+
+if (!isset($_POST["user"]) || !isset($_POST["psw"])) {
+    echo json_encode($responseArray);
+    exit();
+}
+
+try {
+    $db = new DB();
+    $user = $_POST["user"];
+    $psw = $_POST["psw"];
+    $sql = "SELECT nome, cognome, dataNascita, email, telefono, istruttore, status, tipoUtente FROM utenti WHERE username=:user and password=:psw;";
+    $parameters = [
+        [':user', $user],
+        [':psw', $psw]
+    ];
+
+    $result = $db->query($sql, $parameters);
+
+    if ($result && is_array($result) && count($result) > 0) {
+        session_start();
+        $user = $result[0];
+		$_SESSION['nome']=$user['nome'];
+		$_SESSION['cognome']=$user['cognome'];
+		$_SESSION['dataNascita']=$user['dataNascita'];
+		$_SESSION['email']=$user['email'];
+		$_SESSION['telefono']=$user['telefono'];
+		$_SESSION['istruttore']=$user['istruttore'];
+		$_SESSION['status']=$user['status'];
+		$_SESSION['tipoUtente']=$user['tipoUtente'];
+        $responseArray['risultato'] = 'true';
+        $responseArray['utente'] = ($user['tipoUtente'] == 'admin') ? 'admin' : 'user';
+    } else {
+        $responseArray['risultato'] = 'false';
+        $responseArray['tipoErr'] = 'log';
+    }
+
+    echo json_encode($responseArray);
+} catch (PDOException $e) {
+    $responseArray['risultato'] = 'false';
+    $responseArray['tipoErr'] = 'sistema';
+    echo json_encode($responseArray);
+} finally {
+    if (isset($db)) $db->close();
 }
 ?>
