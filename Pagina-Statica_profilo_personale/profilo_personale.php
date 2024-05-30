c988f94cff01ca6eea04ba54035a7e6c04ae32b0
Michele Polini <michele_polini@outlook.com>
2024-05-30 20:43:25 +0200

    Update

--- Pagina-Statica_profilo_personale/profilo_personale.php
+++ Pagina-Statica_profilo_personale/profilo_personale.php
@@ -5,14 +5,16 @@
     <link rel="stylesheet" href="./css/globals.css" />
     <link rel="stylesheet" href="./css/styleguide.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" media="screen and (min-width: 768px)" href="./css/style_profilo_desktop.css">
-    <link rel="stylesheet" media="screen and (max-width: 767px)" href="./css/style_profilo_mobile.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="./js/funzioni.js"></script>
+    <!-- <link rel="stylesheet" media="screen and (min-width: 768px)" href="./css/style_profilo_desktop.css">
+    <link rel="stylesheet" media="screen and (max-width: 767px)" href="./css/style_profilo_mobile.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./js/funzioni.js"></script> -->
 </head>
 <body>
     <div class="header">
         <img class="logo" src="img/immagine-logo-ambulanza.png" />
+        <div class="titolo-profilo"><div class="link">Profilo</div></div>
+        <div class="freccia-dx"><img class="vector" src="img/vector-1.svg" /></div>
         <div id="link">
             <div class="contatti"><div class="link"><a href="contatti.html">Contatti</a></div></div>
             <div class="bacheca"><div class="link"><a href="bacheca.html">Bacheca</a></div></div>
@@ -20,18 +22,16 @@
             <div class="inutile"><div class="link"><a href="profilo.html">Profilo</a></div></div>
             <div class="logout"><div class="link"><a href="logout.html">Log out &gt;</a></div></div>
         </div>
-        <div class="burger-menu">
+        <!-- <div class="burger-menu">
             <div class="menu-mobile">
                 <div id="menu-mobile-box" onclick="showMenu()">
                     <img src="img/burger-menu.svg">
                     <script>stampaMenu("m")</script>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
     <div class="profilo">
-        <div class="titolo-profilo"><div class="link">Profilo</div></div>
-        <div class="freccia-dx"><img class="vector" src="img/vector-1.svg" /></div>
         <div class="immagini">
             <img class="triangolo-arancione" src="img/triangolo-arancione.svg" />
             <img class="triangolo-blu" src="img/triangolo-blu.svg" />
