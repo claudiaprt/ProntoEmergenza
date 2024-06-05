5b7b674c07020baafc7b1f9488936b1e9667fe8d
Mohamed Soliman Hesham <slymanoworld@gmail.com>
2024-06-05 09:23:46 +0200

    240605_092345

--- Pagina-Dinamica_bacheca/function.php
+++ Pagina-Dinamica_bacheca/function.php
@@ -1,31 +1,35 @@
 <?php
-function conn(){
-    $dbname = "pronto_emergenza";   
-    $server="localhost";
-    $username="root";
-    $psw="";
-    return $conn = new PDO("mysql:dbname=$dbname;host=$server",$username,$psw);
+function conn()
+{
+    $dbname = "pronto_emergenza";
+    $server = "localhost";
+    $username = "root";
+    $psw = "";
+    $conn = new PDO("mysql:dbname=$dbname;host=$server", $username, $psw);
+    return $conn;
 }
 
-function select($query,$param=[]){
+function select($query, $param = [])
+{
     $conn = conn();
     $v = [];
-    if($conn != null && isset($query)) {
+    if ($conn != null && isset($query)) {
         $stmt = $conn->prepare($query);
         $stmt->execute($param);
         $v = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $v['rows'] = $stmt->rowCount();
-    }else{
+    } else {
         $v['error'] = "Errore";
     }
-    $conn=null;
+    $conn = null;
     return $v;
 }
 
-function insert($query, $param = []) {
+function insert($query, $param = [])
+{
     $conn = conn();
     $res = [];
-    if ($conn !== null && isset($query)){
+    if ($conn !== null && isset($query)) {
         $stmt = $conn->prepare($query);
         $stmt->execute($param);
         $res['rows'] = $stmt->rowCount();
@@ -35,4 +39,3 @@ function insert($query, $param = []) {
     $conn = null;
     return $res;
 }
-?>
